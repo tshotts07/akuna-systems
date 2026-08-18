052a8fabce11b5728be1bcfffe3c5cac9f7f5293
tshotts07 <tjshotts7@gmail.com>
2026-08-18 05:46:34 -0500

    add 2nd public subnet, tag 1st public subnet

--- main.tf
+++ main.tf
@@ -25,4 +25,19 @@ resource "aws_subnet" "public_1" {
     cidr_block              = "10.0.1.0/24"
     availability_zone       = data.aws_availability_zones.available.names[0]
     map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-1"
+    }
+}
+
+resource "aws_subnet" "public_2" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.2.0/24"
+    availability_zone       = data.aws_availability_zones.available.names[1]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-2"
+    }
 }
