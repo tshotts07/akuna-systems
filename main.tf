cf3f65447ccf35d5a8c05a01b3354a460afde9fe
tshotts07 <tjshotts7@gmail.com>
2026-08-18 05:53:26 -0500

    begin adding public subnet access to internet

--- main.tf
+++ main.tf
@@ -61,3 +61,20 @@ resource "aws_subnet" "private_2" {
         Name = private-subnet-2"
     }
 }
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "public-route-table"
+    }
+}
+
+resource "aws_route_table_association" "public_1" {
+    
+}
