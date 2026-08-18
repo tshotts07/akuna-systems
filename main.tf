803ca10096a3215eafe0d29fd5fa65225fc138eb
tshotts07 <tjshotts7@gmail.com>
2026-08-18 06:05:51 -0500

    add access for pubic 2

--- main.tf
+++ main.tf
@@ -76,5 +76,11 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_1" {
-    
+    subnet_id       = aws_subnet.public_1.id
+    route_table_id  = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
 }
