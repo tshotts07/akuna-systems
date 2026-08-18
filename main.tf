64569ddebcdb2d023b46a132096ae3fec7cd4ff5
tshotts07 <tjshotts7@gmail.com>
2026-08-18 06:21:34 -0500

    create private route table for each availability zone

--- main.tf
+++ main.tf
@@ -122,3 +122,39 @@ resource "aws_nat_gateway" "nat_2" {
         Name = "akuna-nat-gateway-2"
     }
 }
+
+resource "aws_route_table" "private_1" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block          = "0.0.0.0/0"
+        nat_gateway_id      = aws_nat_gateway.nat_1.id
+    }
+
+    tags = {
+        Name = "private-rotue-table-1"
+    }
+}
+
+resource "aws_route_table" "private_2" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block          = "0.0.0.0/0"
+        nat_gateway_id      = aws_nat_gateway.nat_2.id
+    }
+
+    tags = {
+        Name = "private-route-table-2"
+    }
+}
+
+resource "aws_route_table_association" "private_1" {
+    subnet_id       = aws_subnet.private_1.id
+    route_table_id  = aws_route_table.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+    subnet_id       = aws_subnet.private_2.id
+    route_table_id  = aws_route_table.private_2.id
+}
