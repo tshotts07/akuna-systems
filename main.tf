fb2ff9dd67540ae5dec9d9afd9bd0b0ea1c9ff6c
tshotts07 <tjshotts7@gmail.com>
2026-08-18 08:55:57 -0500

    Add security groups, ALB, target group, HTTP listener

--- main.tf
+++ main.tf
@@ -212,5 +212,54 @@ resource "aws_security_group" "ecs" {
 }
 
 resource "aws_lb" "app" {
-    name            = "akuna-web-service-alb"
+    name               = "akuna-web-service-alb"
+    internal           = false
+    load_balancer_type = "application"
+    security_groups    = [aws_security_group.alb.id]
+
+    subnets = [
+        aws_subnet.public_1.id,
+        aws_subnet.public_2.id
+    ]
+
+    enable_deletion_protection = false
+
+    tags = {
+        Name = "akuna-web-service-alb"
+    }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "akuna-web-service-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    path                = "/health"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "akuna-web-service-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
 }
