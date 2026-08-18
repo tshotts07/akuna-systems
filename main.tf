798dbabf7e73cae9275cedac61c9b9ee142395f5
tshotts07 <tjshotts7@gmail.com>
2026-08-18 06:16:59 -0500

    add NAT gateways for outbound access for the private subnets

--- main.tf
+++ main.tf
@@ -84,3 +84,41 @@ resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_eip" "nat_1" {
+    domain = "vpc"
+
+    tags = {
+        Name = "akuna-nat-eip-1"
+    }
+}
+
+resource "aws_eip" "nat_2" {
+    domain - "vpc"
+
+    tags = {
+        Name = "akuna-nat-eip-2"
+    }
+}
+
+resource "aws_nat_gateway" "nat_1" {
+    allocation_id   = aws_eip.nat_1.id
+    subnet_id       = aws_subnet.public_1.id
+
+    depends_on = [aws_internet_gateway.main]
+
+    tags = {
+        Name = "akuna-nat-gateway-1"
+    }
+}
+
+resource "aws_nat_gateway" "nat_2" {
+    allocation_id   = aws_eip.nat_2.id
+    subnet_id       = aws_subnet.public_2.id
+
+    depends_on = [aws_internet_gateway.main]
+
+    tags = {
+        Name = "akuna-nat-gateway-2"
+    }
+}
