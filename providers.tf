d6a6355c746597a39a49d00399ba806695fb27c2
tshotts07 <tjshotts7@gmail.com>
2026-08-19 13:18:27 -0500

    fix providers.tf syntax

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_version = ">= 1.6.0"
+  required_version = ">= 1.6.0"
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 6.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
-provider = "aws" {
-    region = var.aws_region
+provider "aws" {
+  region = var.aws_region
 }
