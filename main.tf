ab8bfde3882b897d6d7aad59162fa189ee40394d
tshotts07 <tjshotts7@gmail.com>
2026-08-19 12:42:21 -0500

    add iam execution role

--- main.tf
+++ main.tf
@@ -294,3 +294,25 @@ resource "aws_ecs_cluster" "main" {
     }
 }
 
+resource "aws_iam_role" "ecs_task_execution" {
+    name = "akuna-ecs-task-execution-role"
+
+    assume_role_policy = jsonecode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Principal = {
+                    Service = "ecs-task.amazonaws.com"
+                }
+                Action = "sts:AssumeRole"
+            }
+        ]
+    })
+}
+
+resouce "aws_iam_role_policy_attachment" "ecs_task_execution" {
+    role        = aws_iam_role.ecs_task_execution.name
+    policy_arm  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy" 
+}
+
